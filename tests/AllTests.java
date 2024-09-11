41976547e4a4d2e6fd3d9aaef27009c93521cef5
BODIN Noe <noe.bodin@imt-atlantique.net>
2024-09-11 22:19:27 +0200

    JUnit fonctionne dans le terminal, avec les scripts

---
+++ tests/AllTests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    sources.MyTest.class  // Add all your test classes here
+})
+public class AllTests {
+}
---
+++ tests/sources/MyTest.java
@@ -0,0 +1,11 @@
+package sources;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyTest {
+    @Test
+    public void testMethod() {
+        assertEquals(1, 1);
+    }
+}
