66088c63ad281be48bd5878d071fb28568a292a6
BODIN Noe <noe.bodin@imt-atlantique.net>
2024-09-12 12:04:42 +0200

    Comments

--- src/destinations/DestinationFinale.java
+++ src/destinations/DestinationFinale.java
@@ -7,10 +7,20 @@ import information.InformationNonConformeException;
  * Classe concrète représentant une destination qui reçoit des informations de type Boolean
  */
 public class DestinationFinale extends Destination<Boolean> {
+	
+	/**
+	 * Constructeur de la classe DestinationFinale
+	 */
+	public DestinationFinale() {
+		super();
+	}
 
-    /**
-     * Constructeur par défaut
-     */
+	/**
+	 * Méthode permettant de recevoir une information de type Boolean
+	 * 
+	 * @param information L'information reçue
+	 * @throws InformationNonConformeException si l'Information comporte une anomalie
+	 */
     public void recevoir(Information<Boolean> information) throws InformationNonConformeException {
         if (information == null || information.nbElements() == 0) {
             throw new InformationNonConformeException("L'information est vide ou non conforme.");
